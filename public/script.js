20df9f3dc97520cf97ddc62dbdeb1ac279c768a1
Mare <Marmare@agea.com.ar>
2023-08-28 18:24:18 -0300

    Libre terminado

--- public/script.js
+++ public/script.js
@@ -1,24 +1,52 @@
-/* Calculos CPM */
+/* CALCULOS CPM */
+const botonCalcular = document.getElementById("calcular");
+const impresiones = document.getElementById("impresiones");
+const botonReset = document.getElementById("reset");
+const bloqueCalculos = document.getElementById("bloque-calculos");
+const contenidoOriginal = bloqueCalculos.innerHTML;
 
-const botonCalcular = document.getElementById('calcular');
-const impresiones = document.getElementById('impresiones');
-
-/* Tarifario */
-botonCalcular.addEventListener('click', () => {
-let cpmTarifario = parseFloat(impresiones.value);
-cpmTarifario = ((cpmTarifario * 80) / 1000).toLocaleString();
-
-const resultado1 = document.getElementById('cpmTarifario');
-resultado1.textContent = cpmTarifario;
-
-/* Piso */
-let cpmPiso = parseFloat(impresiones.value);
-cpmPiso = ((cpmPiso * 25) / 1000).toLocaleString();
-
-const resultado2 = document.getElementById('cpmPiso');
-resultado2.textContent = cpmPiso;
+/* Boton reset */
+botonReset.addEventListener("click", () => {
+  impresiones.value = "";
 });
 
-/* Libre */
+botonReset.addEventListener("click", () => {
+  bloqueCalculos.innerHTML = contenidoOriginal;
+});
 
-/* Porcentaje */
+/* Tarifario */
+botonCalcular.addEventListener("click", () => {
+  let cpmTarifario = parseFloat(impresiones.value);
+  cpmTarifario = ((cpmTarifario * 80) / 1000).toLocaleString();
+
+  const resultado1 = document.getElementById("cpmTarifario");
+  resultado1.textContent = cpmTarifario;
+
+/* Piso */
+  let cpmPiso = parseFloat(impresiones.value);
+  cpmPiso = ((cpmPiso * 25) / 1000).toLocaleString();
+
+  const resultado2 = document.getElementById("cpmPiso");
+  resultado2.textContent = cpmPiso;
+});
+
+/* Libre + porcentaje de diferencia*/
+const cpmLibre = document.getElementById('valorinput');
+const botonIncrementar = document.getElementById('botonIncrementar');
+const botonDecrementar = document.getElementById('botonDecrementar');
+
+botonIncrementar.addEventListener('click', () => {
+  cpmLibre.value = parseInt(cpmLibre.value) + 1;
+ });
+
+botonDecrementar.addEventListener('click', () => {
+  cpmLibre.value = parseInt(cpmLibre.value) - 1;
+ });
+
+botonCalcular.addEventListener("click", () => {
+  cpmLibre2 = cpmLibre.value;
+  resultadoLibre = ((impresiones.value * cpmLibre2) / 1000).toLocaleString();
+
+  const resultado3 = document.getElementById('cpmLibre');
+   resultado3.textContent = resultadoLibre;
+});
